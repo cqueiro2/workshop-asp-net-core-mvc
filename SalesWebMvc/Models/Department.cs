e21892bd12b0d3e76d64e105d8f0a5ea5f4dc508
Cesario A Queiroz <cqueiro2@hotmail.com>
2018-11-10 17:51:19 -0200

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -9,5 +9,31 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()//construtor padrão é necessario
+        {
+
+        }
+
+        public Department(int id, string name)//retire os contrutor Collection
+        {
+            Id = id;
+            Name = name;
+           
+        }
+
+        public void AddSeller(Seller seller)
+        {
+
+            Sellers.Add(seller);
+
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
